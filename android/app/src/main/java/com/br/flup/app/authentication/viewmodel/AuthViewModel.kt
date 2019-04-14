757b1ac2913d434b89e36e2c12a5dbff1458c8c2
Pedro Belfort <belfortdev@gmail.com>
2019-04-14 19:55:39 -0300

    Introduce AuthViewModel and ViewModel core structre

---
+++ android/app/src/main/java/com/br/flup/app/authentication/viewmodel/AuthViewModel.kt
@@ -0,0 +1,35 @@
+package com.br.flup.app.authentication.viewmodel
+
+import androidx.lifecycle.LiveData
+import com.belfortdev.hurbchallenge.core.extension.toLiveData
+import com.br.flup.app.authentication.data.SessionRepository
+import com.br.flup.app.authentication.model.SignInForm
+import com.br.flup.app.authentication.model.SignInResult
+import com.br.flup.app.core.data.Outcome
+import com.br.flup.app.core.viewmodel.DisposingViewModel
+
+class AuthViewModel : DisposingViewModel() {
+
+    private val repo = SessionRepository(compositeDisposable)
+
+    val signInEventOutcome: LiveData<Outcome<SignInResult>> by lazy {
+        repo.signInEventOutcome.toLiveData(compositeDisposable)
+    }
+
+    val signInEmployeeOutcome: LiveData<Outcome<SignInResult>> by lazy {
+        repo.signInEmployeeOutcome.toLiveData(compositeDisposable)
+    }
+
+    fun signInEvent(signInForm: SignInForm) {
+        if (signInEventOutcome.value == null) {
+            repo.signInEvent(signInForm)
+        }
+    }
+
+    fun signInEmployee(signInForm: SignInForm) {
+        if (signInEventOutcome.value == null) {
+            repo.signInEmployee(signInForm)
+        }
+    }
+
+}

0233c4faab2d8ee430953f3579025702c2a963bd
Pedro Belfort <belfortdev@gmail.com>
2019-04-18 16:05:11 -0300

    Conclude first form scene transition animation

--- android/app/src/main/java/com/br/flup/app/authentication/ui/AuthFragment.kt
+++ android/app/src/main/java/com/br/flup/app/authentication/ui/AuthFragment.kt
@@ -6,15 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.interpolator.view.animation.FastOutLinearInInterpolator
 import androidx.transition.Fade
+import androidx.transition.Scene
 import androidx.transition.TransitionManager
 import androidx.transition.TransitionSet
+import com.br.flup.app.R
 import com.br.flup.app.authentication.viewmodel.AuthViewModel
 import com.br.flup.app.core.extension.getViewModel
 import com.transitionseverywhere.extra.Scale
+import kotlinx.android.synthetic.main.auth_event_form_scene.*
 import kotlinx.android.synthetic.main.auth_fragment.*
 
 
@@ -28,13 +30,18 @@ class AuthFragment : Fragment() {
         getViewModel { AuthViewModel() }
     }
 
+    private lateinit var mFormEventScene: Scene
+    private lateinit var mFormEmployeeScene: Scene
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(com.br.flup.app.R.layout.auth_fragment, container, false)
+        return inflater.inflate(R.layout.auth_fragment, container, false)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         setupViewListeners()
+        setupScenes()
         // Use viewModel here
     }
 
@@ -46,18 +53,32 @@ class AuthFragment : Fragment() {
         authFormFAB.setOnClickListener { onFabClick() }
     }
 
+    private fun setupScenes() {
+        mFormEventScene = Scene(formRootScene)
+        mFormEmployeeScene = Scene.getSceneForLayout(formRootScene, R.layout.auth_employee_form_scene, requireContext())
+    }
+
     private fun onFabClick() {
-        val transitionSet = TransitionSet()
-            .addTransition(Scale(1.2f))
-            .addTransition(Fade())
-            .setInterpolator(FastOutLinearInInterpolator())
-        TransitionManager.beginDelayedTransition(authContainer, transitionSet)
-        authRearFormView.visibility = if (authRearFormView.isVisible) View.INVISIBLE else View.VISIBLE
+        animateFormTransition()
     }
 
     private fun hideKeyboard() {
         val inputMethodManager = context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
+    }
+
+    private fun animateFormTransition() {
+        val transitionSet = TransitionSet()
+        val scaleTransition = Scale(1.2f).addTarget(authEventFormView)
+        val fadeTransition = Fade().addTarget(authEventFormView)
+
+        transitionSet
+            .addTransition(fadeTransition)
+            .addTransition(scaleTransition)
+        transitionSet.interpolator = FastOutLinearInInterpolator()
+        transitionSet.ordering = TransitionSet.ORDERING_TOGETHER
+
+        TransitionManager.go(mFormEmployeeScene, transitionSet)
     }
 
 }

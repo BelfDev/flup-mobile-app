4a61fec23c20a832385c9780bcf2912417d0e698
Pedro Belfort <belfortdev@gmail.com>
2019-04-18 11:09:46 -0300

    Add auth form focus behavior

--- android/app/src/main/java/com/br/flup/app/authentication/ui/AuthFragment.kt
+++ android/app/src/main/java/com/br/flup/app/authentication/ui/AuthFragment.kt
@@ -1,13 +1,17 @@
 package com.br.flup.app.authentication.ui
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
-import com.br.flup.app.R
 import com.br.flup.app.authentication.viewmodel.AuthViewModel
 import com.br.flup.app.core.extension.getViewModel
+import kotlinx.android.synthetic.main.auth_form_view.*
+import kotlinx.android.synthetic.main.auth_fragment.*
+
 
 class AuthFragment : Fragment() {
 
@@ -20,12 +24,25 @@ class AuthFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.auth_fragment, container, false)
+        return inflater.inflate(com.br.flup.app.R.layout.auth_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        setupViewListeners()
         // Use viewModel here
+    }
+
+    private fun setupViewListeners() {
+        authenticationContainer.setOnTouchListener { _, _ ->
+            hideKeyboard()
+            false
+        }
+    }
+
+    private fun hideKeyboard() {
+        val inputMethodManager = context?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 
 }
